f9392e6b7e8359f3b2dd9816b7fd1d4ec143df64
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 08:54:37 +0100

    wallet micro-service :  add entities

---
+++ wallet-service/src/main/java/loukili/imane/walletservice/entities/Client.java
@@ -0,0 +1,20 @@
+package loukili.imane.walletservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @ToString
+public class Client {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private String prenom;
+    @OneToMany(mappedBy = "client")
+    private List<Wallet> wallets;
+}
---
+++ wallet-service/src/main/java/loukili/imane/walletservice/entities/Wallet.java
@@ -0,0 +1,25 @@
+package loukili.imane.walletservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data  @NoArgsConstructor  @AllArgsConstructor  @ToString
+public class Wallet {
+    @Id @GeneratedValue
+    private UUID id;
+    private double solde;
+    private Date dateCréation;
+    private String devise;
+    @ManyToOne
+    private Client client;
+}
