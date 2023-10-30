17d2af7a21a0e6e90c948d629c6ada6112bc9250
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 09:27:16 +0100

    gateway-service  : update

--- gateway-service/src/main/java/loukili/imane/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/loukili/imane/gatewayservice/GatewayServiceApplication.java
@@ -13,8 +13,6 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-
-
 	//dynamic routes configuration with Discovery Service
 	@Bean
 	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
