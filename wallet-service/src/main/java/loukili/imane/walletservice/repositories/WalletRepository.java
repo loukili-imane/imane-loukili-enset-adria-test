f7e1e0d67315fd1f0968010af1f8b5b0b2235337
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 09:25:52 +0100

    wallet micro-service :  update repository

--- wallet-service/src/main/java/loukili/imane/walletservice/repositories/WalletRepository.java
+++ wallet-service/src/main/java/loukili/imane/walletservice/repositories/WalletRepository.java
@@ -2,11 +2,17 @@ package loukili.imane.walletservice.repositories;
 
 import loukili.imane.walletservice.entities.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource
 public interface WalletRepository extends JpaRepository<Wallet, UUID>{
-    Wallet findWalletById(UUID id);
+    @RestResource(path = "/{wallet}")
+    Wallet findWalletById(@Param("wallet") UUID id);
+
+
 }
