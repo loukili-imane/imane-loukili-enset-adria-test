0a19c27158dfc3399b1f617f02e6c266c7442994
loukili-imane <imane.loukili2115@gmail.com>
2023-10-30 08:16:43 +0100

    add transfer micro-service

---
+++ transfer-service/src/test/java/loukili/imane/transferservice/TransferServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package loukili.imane.transferservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
